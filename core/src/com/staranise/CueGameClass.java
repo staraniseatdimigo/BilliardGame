8ecbcaf803112dc0eea024b1c6d37aa5cae1c6a9
현성 <hyunsnug@naver.com>
2016-04-09 18:27:13 +0900

    Graphic Class Updated

--- core/src/com/staranise/CueGameClass.java
+++ core/src/com/staranise/CueGameClass.java
@@ -3,25 +3,23 @@ package com.staranise;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.staranise.Basic.ObjectManager;
+import com.staranise.Basic.QueObject;
 
 public class CueGameClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        QueObject ball1 = new QueObject("./core/assets/Ball1.png", 50.f, 0.f);
+        QueObject ball2 = new QueObject("./core/assets/Ball2.png", 60.f, 50.f);
+        QueObject ball3 = new QueObject("./core/assets/Ball3.png", 88.f, 30.f);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.75f, 0.75f, 0.75f, 1.f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+
+        ObjectManager.getInstance().Render();
+    }
 }
