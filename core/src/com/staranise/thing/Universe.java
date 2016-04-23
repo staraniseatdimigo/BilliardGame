159a04024df08471eac89bc820cc09e4de74147a
parkyutack <parkyutack12@naver.com>
2016-04-23 16:18:43 +0900

    Vec2 수정

--- core/src/com/staranise/thing/Universe.java
+++ core/src/com/staranise/thing/Universe.java
@@ -62,24 +62,21 @@ public class Universe {
 
                     //한 물체가 정지한 경우에만 적용됨 테스트 코드에선 움직이는 thing을 먼저 넣고 그다음 안움직이는걸 넣는 걸로
                     //t1이 처음 움직이던 물체, t2가 멈춰있던 물체
-                    Vec2 v0 = t1.getLinearSpeed(); // 움직이는 물체 처음속도
+                    Vec2 v0 = t1.getLinearSpeed().minus(t2.getLinearSpeed()); // 좌표계 변환 t1이 정지한 물체로
+                    Vec2 v2Direct = t2.getPosition().minus(t1.getPosition()).norm(); // 부딪힌 물체의 방향벡터 두 물체의 중심 위치를 빼서 구함
 
-                    Vec2 t2Pos = t2.getPosition();
-
-                    Vec2 v2Direct = Vec2.minus(t2Pos, t1.getPosition()).getNorm(); // 부딪힌 물체의 방향벡터 두 물체의 중심 위치를 빼서 구함
-
-                    float v0v2cos = v0.getCos(Vec2.minus(t2Pos, t1.getPosition())); // 움직이는 물체의 방향과 부딪힌 물체와의 사이각
+                    float v0v2cos = v0.getCos(t2.getPosition().minus(t1.getPosition())); // 움직이는 물체의 방향과 부딪힌 물체와의 사이각
                     System.out.println("------------------------------------------" + v0v2cos);
 
-                    float v2Length = Vec2.multi(v0.multi(v0v2cos).getLength()); // v0의 크기와 사이각을 통해 v2에 길이를 구함
+                    float v2Length = v0.multi(v0v2cos).getLength(); // v0의 크기와 사이각을 통해 v2에 길이를 구함
                     System.out.println("------------------------------------------" + v2Length);
 
                     Vec2 v2 = v2Direct.multi(v2Length); //v2방향와 크기로 v2를 구함
                     Vec2 v1 = new Vec2(v0.x - v2.x, v0.y - v2.y); // v1 + v2 = v0을 이용해 v1을 구함
 
-                    t1.setLinearSpeed(v1);
+                    t1.setLinearSpeed(v1.add(t2.getLinearSpeed()));
                     System.out.println("------------------------------------------" + v1);
-                    t2.setLinearSpeed(v2);
+                    t2.setLinearSpeed(v2.add(t2.getLinearSpeed()));
                     System.out.println("------------------------------------------" + v2);
                 }
             }
